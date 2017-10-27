29c5407acef78635fd898649442c30e62d55b378
Alexandru Duma <badco25@gmail.com>
2017-10-27 22:22:27 +0300

    Added TrainersController. Implemented add and delete methods and edit method skeleton

---
+++ Gym Application/DAL/Repository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DAL.Model;
+
+namespace DAL.Repository
+{
+    internal class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseModel
+    {
+        internal GymDBContext context;
+        internal DbSet<TEntity> dbSet;
+
+        public Repository(GymDBContext ctx)
+        {
+            context = ctx;
+            dbSet = context.Set<TEntity>();
+        }
+
+        public virtual void Save(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+        public TEntity Delete(TEntity entity)
+        {
+            TEntity deleted = dbSet.Remove(entity);
+            return deleted;
+        }
+        public void Update(TEntity entity)
+        {
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public IEnumerable<TEntity> findAll()
+        {
+            IQueryable<TEntity> All = dbSet;
+            return All.ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            TEntity entity = dbSet.Find(id);
+            return entity;
+        }
+
+        public IQueryable<TEntity> GetQueryable()
+        {
+            return dbSet;
+        }
+    }
+}
---
+++ Gym Application/DAL/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Model;
+
+namespace DAL.Repository
+{
+    public class UnitOfWork : IDisposable
+    {
+        private GymDBContext context;
+        private Dictionary<Type, object> repositories;
+
+        public UnitOfWork()
+        {
+            context = new GymDBContext();
+            repositories = new Dictionary<Type, object>();
+        }
+
+        public IRepository<T> Repository<T>() where T : BaseModel
+        {
+            var typeKey = typeof(T);
+            if (!repositories.ContainsKey(typeKey))
+                repositories.Add(typeKey, new Repository<T>(context));
+            return repositories[typeof(T)] as IRepository<T>;
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
