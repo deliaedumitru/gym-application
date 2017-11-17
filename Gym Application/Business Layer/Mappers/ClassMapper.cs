4625b3ba6ee4bb0da25f7338d906f96295cd4b42
ioanacotutiu <ioanacotutiu31@gmail.com>
2017-11-17 15:47:16 +0200

    Add files via upload

---
+++ Gym Application/Business Layer/Mappers/ClassMapper.cs
@@ -0,0 +1,28 @@
+﻿using Business_Layer.DTO;
+using DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.Mappers
+{
+    public class ClassMapper
+    {
+        public Class ClassMVToClass(ClassModelView model)
+        {
+            Class classM = new Class();
+            classM.Name = model.Name;
+            return classM;
+        }
+
+        public BaseClassModelView ClassToBaseClassMV(Class classM)
+        {
+            BaseClassModelView model = new BaseClassModelView();
+            model.Id = classM.Id;
+            model.Name = classM.Name;
+            return model;
+        }
+    }
+}
