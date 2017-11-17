8abdf73e8fd8715aa951378fd5a0cf7bcf66b3e0
ioanacotutiu <ioanacotutiu31@gmail.com>
2017-11-17 15:48:23 +0200

    Add files via upload

---
+++ Gym Application/Business Layer/DTO/ClassModelView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.DTO
+{
+    public class ClassModelView
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+    }
+
+    public class BaseClassModelView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
