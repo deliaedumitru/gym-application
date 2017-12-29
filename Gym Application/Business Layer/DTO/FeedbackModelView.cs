1baf2df983d1c41954beede5837d0963df357515
ioanacotutiu <ioanacotutiu31@gmail.com>
2017-12-29 17:20:06 +0200

    Added trainer feedback backend

---
+++ Gym Application/Business Layer/DTO/FeedbackModelView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Layer.DTO
+{
+    public class FeedbackModelView
+    {
+        public int TrainerId { get; set; }
+
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(1500)]
+        public string Text { get; set; }
+
+        public short Rating { get; set; }
+    }
+
+        public class BaseFeedbackModelView
+        {
+            public int Id { get; set; }
+            public int TrainerId { get; set; }
+
+            public int UserId { get; set; }
+
+            [Required]
+            [StringLength(1500)]
+            public string Text { get; set; }
+
+            public short Rating { get; set; }
+    
+        }
+}
